d49242e321c9d565bd96b01ca8ce1dea869ac156
avellar1975 <evandro.avellar@gmail.com>
2020-09-05 13:27:13 -0300

    Inclusão do parametrize nos testes do pytest

--- tests/test_primos.py
+++ tests/test_primos.py
@@ -1,15 +1,31 @@
+"""Testes para as funções."""
 from primoj.funcoes import primos_divisores, verifica_primo, lista_primos_ate
+import pytest
+
 
 def test_primos_divisores():
     esperado = [2, 3]
     resultado = primos_divisores(10)
     assert resultado == esperado
 
-def test_verifica_primo():
-    falso = verifica_primo(9)
-    verdadeiro = verifica_primo(17)
-    assert falso == False
-    assert verdadeiro == True
+
+@pytest.mark.parametrize(
+    'numeroj_p',
+    [13, 17, 97, 101]
+)
+def test_verifica_primo_verdadeiro(numeroj_p):
+    verdadeiro = verifica_primo(numeroj_p)
+    assert verdadeiro is True
+
+
+@pytest.mark.parametrize(
+    'numeroj_n',
+    [4, 16, 98, 102]
+)
+def test_verifica_primo_falso(numeroj_n):
+    falso = verifica_primo(numeroj_n)
+    assert falso is False
+
 
 def test_lista_primos_ate():
     lista = lista_primos_ate(30)
