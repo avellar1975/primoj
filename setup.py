b80f7aefcc09976bc8488f1ab93872fcb1f18b5e
avellar1975 <evandro.avellar@gmail.com>
2020-09-05 10:22:33 -0300

    Configurando setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,17 @@ setup(
     author_email='evandro.avellar@gmail.com',
     packages=['primoj'],
     description='Descubra números primos',
-    long_description=README.md,
-    licence=LICENSE,
+    long_description='primoj(primos em esperanto) trabalhar com 3 funções'
+    + ' para utilizar em números primos.',
+    licence='GNU Affero General Public License v3 or later (AGPLv3+)',
     url = 'https://github.com/avellar1975/primoj'
 )
+classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Scientific/Engineering :: Physics'
+    ]
